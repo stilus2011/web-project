b87e94fd6dd050d6e225d4e99bfbc604310717ef
roman <stilus-2011@mail.ru>
2016-05-14 12:56:22 +0300

    ch

--- init.sh
+++ init.sh
@@ -1,12 +1,2 @@
-# Nginx
-if [ -f /etc/nginx/sites-enabled/default ]; then
-  sudo rm /etc/nginx/sites-enabled/default
-fi
-touch /home/box/nginx.log
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-# Gunicorn (ver. 17.5)
-touch /home/box/gunicorn.log
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
